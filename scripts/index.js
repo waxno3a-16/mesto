6044442e47b4f61fcb84f0256abd68d0f5da263a
Shakhnoza Rakhimova <waxno3a@yandex.ru>
2023-03-08 23:02:39 +0300

    Добавлен попап открытой картинки

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,6 @@ let formElementAdd = document.querySelector('#addPopupForm');
 let cardNameInput = document.querySelector('#addPopupForm input[name="add-name"]');
 let cardLinkInput = document.querySelector('#addPopupForm input[name="add-link"]');
 
-
 //открываем попап редактирования профиля//
 function openPopup() {
   profileEdit.classList.add('popup_opened');
@@ -72,6 +71,7 @@ const initialCards = [
 ];
 
 const cardsContainer = document.querySelector('.container');
+
 //выводим массив карточек-фотографий//
 
 const createCard = (card) => {
@@ -82,8 +82,25 @@ const createCard = (card) => {
   cardImage.setAttribute('src', card.link);
   cardImage.setAttribute('alt', card.alt);
   let cardLikeButton = newCard.querySelector('.container__button');
-  function likeCard(){cardLikeButton.classList.toggle('container__button_active');};
+  function likeCard(){cardLikeButton.classList.toggle('container__button_active');};//добавляем и убираем лайк к карточкам//
   cardLikeButton.addEventListener('click', likeCard);
+  let cardDeleteButton = newCard.querySelector('.container__deletebtn');
+  function deleteCard(evt){evt.target.closest('.container__element').remove();};//удаляем карточку по клику на корзину
+  cardDeleteButton.addEventListener('click', deleteCard);
+  //открываем большое изображение и передаем картинку и алт
+  
+  function openCard(evt){
+    let popupImage = document.querySelector('.popup__image');
+    let popupHeading = document.querySelector('.popup__image-name');
+    popupImage.src = evt.target.src;
+    popupHeading.textContent = evt.target.alt;
+    document.querySelector('#imagePopup').classList.add('popup_opened');
+  };
+  let popupImageOpened = newCard.querySelector('.container__image');
+  popupImageOpened.addEventListener('click', openCard);
+  let popupImageClose = document.querySelector('#closeImagePopup');
+  popupImageClose.addEventListener('click', function(){document.querySelector('#imagePopup').classList.remove('popup_opened');});
+
   return newCard;
 }
 
@@ -95,7 +112,7 @@ const renderCard = (card, cardsContainer) => {
 initialCards.forEach(data => {renderCard(data, cardsContainer);});
 
 //изменяем в форме название и ссылку и добавляем новую карточку по клику на сабмит//
-function picFormSubmit (evt) {
+function cardFormSubmit (evt) {
   evt.preventDefault();
   renderCard({
     name: cardNameInput.value,
@@ -117,7 +134,7 @@ function closePopupAdd() {
 
 profileAddButton.addEventListener('click', openPopupAdd);
 profileCloseAddButton.addEventListener('click', closePopupAdd);
-formElementAdd.addEventListener('submit', picFormSubmit);
+formElementAdd.addEventListener('submit', cardFormSubmit);
 
-//добавляем и убираем лайк к карточкам//
+
 
